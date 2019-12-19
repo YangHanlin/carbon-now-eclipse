639982af9aac777ef67ea16a5ea0d2ac1574495e
YangHanlin <mattoncis@hotmail.com>
2019-12-19 21:59:49 +0800

    Fix the url error with complex content in editor

--- src/com/carbon/now/eclipse/service/impl/InvokeCarbonNowServiceViaBrowserImpl.java
+++ src/com/carbon/now/eclipse/service/impl/InvokeCarbonNowServiceViaBrowserImpl.java
@@ -1,5 +1,7 @@
 package com.carbon.now.eclipse.service.impl;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 import com.carbon.now.eclipse.entity.CarbonNowRequest;
@@ -10,10 +12,18 @@ import com.carbon.now.eclipse.service.WebBrowserService;
 public class InvokeCarbonNowServiceViaBrowserImpl implements InvokeCarbonNowService {
 	static private final String carbonNowUrl = "https://carbon.now.sh/";
 	
-	static private String toArgStr(Map<String, String> args) {
+	static private String encode(String str) throws CarbonNowException {
+		try {
+			return URLEncoder.encode(str, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new CarbonNowException("Internal exception: unsupported encoding");
+		}
+	}
+	
+	static private String toArgStr(Map<String, String> args) throws CarbonNowException {
 		StringBuilder argStrBuilder = new StringBuilder("?");
 		for (String key : args.keySet())
-			argStrBuilder.append(key + "=" + args.get(key) + "&");
+			argStrBuilder.append(encode(key) + "=" + encode(args.get(key)) + "&");
 		argStrBuilder.deleteCharAt(argStrBuilder.length() - 1);
 		return argStrBuilder.toString();
 	}
